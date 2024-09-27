a5adb3aac581ce0a6bf1098f42910fd5edb8c20e
Arkadiy <laeis88@gmail.com>
2024-09-27 23:31:54 +0300

    Fixed homework 18

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -3,95 +3,56 @@ export const useAccordionFoot = () => {
   const footerItemTitle = document.querySelectorAll('.footer__item-title');
   const footerSublist = document.querySelectorAll('.footer__sublist');
 
-  if (footerItem.length === 0 || footerItemTitle.length === 0 || footerSublist.length === 0) {
-        return;
-      }
-      
+  const widthWindow = window.innerWidth;
   const width = 576;
 
-  const addClassFooter = () => {
-
-    footerItem.forEach((el) => {
-        el.classList.add('accordion-list');
-    });
-  
-    footerItemTitle.forEach((el) => {
-        el.classList.add('accordion-list__control');
-    });
-  
-    footerSublist.forEach((el) => {
-      el.classList.add('accordion-list__content')
-    });
-  }
-
-  const removeClassFooter = () => {
-    footerItem.forEach((el) => {
-      el.classList.remove('accordion-list');
+  footerItem.forEach((el) => {
+    if (widthWindow <= width) {
+      el.classList.add('accordion-list');
+    }
   });
 
   footerItemTitle.forEach((el) => {
-      el.classList.remove('accordion-list__control');
+    if (widthWindow <= width) {
+      el.classList.add('accordion-list__control');
+    }
   });
 
   footerSublist.forEach((el) => {
-    el.classList.remove('accordion-list__content')
-  });
-  }
-  
-
-  
-  const addEventListeners = () => {
-    const accordionLists = document.querySelectorAll('.accordion-list');
-    accordionLists.forEach((el) => {
-      el.addEventListener('click', (e) => {
-        console.log('click')
-        const accordionList = e.currentTarget;
-        const accordionOpenedItem = accordionList.querySelector(
-          '.accordion-list__item--opened',
-        );
-        const accordionOpenedContent = accordionList.querySelector(
-          '.accordion-list__item--opened .accordion-list__content',
-        );
-  
-        const accordionControl = e.target.closest('.accordion-list__control');
-        if (!accordionControl) return;
-        e.preventDefault();
-        const accordionItem = accordionControl.parentElement;
-        const accordionContent = accordionControl.nextElementSibling;
-  
-        if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
-          accordionOpenedItem.classList.remove('accordion-list__item--opened');
-          accordionOpenedContent.style.maxHeight = null;
-        }
-  
-        accordionItem.classList.toggle('accordion-list__item--opened');
-  
-        if (accordionItem.classList.contains('accordion-list__item--opened')) {
-          accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-        } else {
-          accordionContent.style.maxHeight = null;
-        }
-      });
-    });
-  }
-
-    const removeEventListeners = () => {
-    accordionLists.forEach((el) => {
-      el.removeEventListener('click', () => {});
-    });
-  };
-
-  const mobileAccordion = () => {
-    if(window.innerWidth <= width) {
-      addClassFooter();
-      addEventListeners();
-    } else {
-      removeClassFooter();
-      removeEventListeners();
+    if (widthWindow <= width) {
+      el.classList.add('accordion-list__content');
     }
-  }
+  });
 
-  window.addEventListener('resize', mobileAccordion);
-  mobileAccordion();
+  const accordionLists = document.querySelectorAll('.accordion-list');
+  accordionLists.forEach((el) => {
+    el.addEventListener('click', (e) => {
+      const accordionList = e.currentTarget;
+      const accordionOpenedItem = accordionList.querySelector(
+        '.accordion-list__item--opened',
+      );
+      const accordionOpenedContent = accordionList.querySelector(
+        '.accordion-list__item--opened .accordion-list__content',
+      );
 
+      const accordionControl = e.target.closest('.accordion-list__control');
+      if (!accordionControl) return;
+      e.preventDefault();
+      const accordionItem = accordionControl.parentElement;
+      const accordionContent = accordionControl.nextElementSibling;
+
+      if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+        accordionOpenedItem.classList.remove('accordion-list__item--opened');
+        accordionOpenedContent.style.maxHeight = null;
+      }
+
+      accordionItem.classList.toggle('accordion-list__item--opened');
+
+      if (accordionItem.classList.contains('accordion-list__item--opened')) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else {
+        accordionContent.style.maxHeight = null;
+      }
+    });
+  });
 };
