49d9af7efed050f166d3cfe6429f0a61e063b0e6
Arkadiy <laeis88@gmail.com>
2024-09-28 01:25:27 +0300

    Created omework 19

--- src/js/components/pricing/priceSwitcher.js
+++ src/js/components/pricing/priceSwitcher.js
@@ -3,18 +3,23 @@ export const usePriceSwitcher = () => {
   const starterPrice = document.querySelector('[data-price="starter"]');
   const popularPrice = document.querySelector('[data-price="popular"]');
   const enterprisePrice = document.querySelector('[data-price="enterprise"]');
+  const periodSt = document.querySelector('[data-period="starter"]');
+  const periodPop = document.querySelector('[data-period="popular"]');
+  const periodEnt = document.querySelector('[data-period="enterprise"]');
   const priceList = {
     starter: {
-      default: 120,
-      withSale: 100,
+      default:'$120',
+      withSale: '$1200',
+      month: '/mo',
+      year: '/yr',
     },
     popular: {
-      default: 1680,
-      withSale: 1400,
+      default: '$1680',
+      withSale: '$16800',
     },
     enterprise: {
-      default: 2520,
-      withSale: 2100,
+      default: '$2520',
+      withSale: '$25200',
     },
   };
 
@@ -22,12 +27,18 @@ export const usePriceSwitcher = () => {
     starterPrice.textContent = priceList.starter.withSale;
     popularPrice.textContent = priceList.popular.withSale;
     enterprisePrice.textContent = priceList.enterprise.withSale;
+    periodSt.textContent = priceList.starter.year;
+    periodPop.textContent = priceList.starter.year;
+    periodEnt.textContent = priceList.starter.year;
   };
 
   const setDefaultPrices = () => {
     starterPrice.textContent = priceList.starter.default;
     popularPrice.textContent = priceList.popular.default;
     enterprisePrice.textContent = priceList.enterprise.default;
+    periodSt.textContent = priceList.starter.month;
+    periodPop.textContent = priceList.starter.month;
+    periodEnt.textContent = priceList.starter.month;
   };
 
   switcher.checked = true;
